bef98f0b0e634f289434017ecc7f33ddb37d4414
Abdimalik-dev <cabdixuseen076@gmail.com>
2023-10-27 10:15:46 +0300

    Challenge completed

--- challenge.js
+++ challenge.js
@@ -1,2 +1,25 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+function cal(Var1, Var2, operator) {
+    if (operator === "+") {
+      return Var1 + Var2;
+    } else if (operator === "-") {
+      return Var1 - Var2;
+    } else if (operator === "") {
+      return Var1 + Var2;
+    } else if (operator === "/") {
+      if (Var2 === 0) {
+        return "Cannot divide by zero";
+      } else {
+        return Var1 / Var2;
+      }
+    } else {
+      return "Invalid operator";
+    }
+  }
+  
+  
+  console.log(cal(2, 2, "+"));t: 
+  console.log(cal(9, 3, "-")); 
+  console.log(cal(9, 20, "*")); 
+  console.log(cal(10, 4, "/")); 
+  console.log(cal(4, 5, "/")); 
+  console.log(cal(40, 6, "%")); 
